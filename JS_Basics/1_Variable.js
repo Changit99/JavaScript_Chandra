09446a026d8741e679fe2b2f7264ff2a9e5fa8b8
Changit99 <144887801+Changit99@users.noreply.github.com>
2024-04-09 08:22:39 +0000

    Variable

---
+++ JS_Basics/1_Variable.js
@@ -0,0 +1,8 @@
+const accountId=644162;
+let accountName="Chandra Kanta Ghosh";
+var accountEmail="ckg@google.com";
+accountCity="BBSR"
+console.log(accountId+accountEmail);
+accountEmail="ckg@gmail.com"
+accountCity="Suri"
+console.table([accountId,accountName,accountEmail,accountCity]);
