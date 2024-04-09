d66e657349422bd345d056205210a6dba994a775
Changit99 <144887801+Changit99@users.noreply.github.com>
2024-04-09 08:41:08 +0000

    dataTypes

---
+++ JS_Basics/02_dataType.js
@@ -0,0 +1,17 @@
+//tc39.es scripting specification
+
+let name="hitesh"
+let age=18
+let isloggedIn=true
+
+//number =>2^53 range
+//bigint
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefined =void
+//symbol= unique
+
+//object  *null is a object
+
+console.log(typeof "Chandra");
