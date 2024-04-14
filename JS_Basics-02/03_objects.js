22472473ce9c45495f2acd8e41fcbdb696c277c4
Changit99 <144887801+Changit99@users.noreply.github.com>
2024-04-14 13:00:20 +0000

    objects

---
+++ JS_Basics-02/03_objects.js
@@ -0,0 +1,16 @@
+const jsUser={
+    name:"Chandra",
+    Email:"chandrakantaghosh81@gmail.com",
+    profile:"Test Analyst",
+    location:["Kolkata","Bhubaneswar"]
+
+}
+
+console.log(jsUser.name);
+console.log(jsUser["location"]);
+console.log(jsUser["profile"]);
+
+jsUser.greetings=function() {
+    console.log(`Welcome ${this.name} your Email is ${this.Email}`);  
+}
+console.log(jsUser.greetings());
